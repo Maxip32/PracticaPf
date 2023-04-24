ef19a1da8e5e055c52f7e3c24451e41e40e72a56
Ana Studler <anistudler@gmail.com>
2023-04-24 14:29:26 -0300

    Agregue un div y un p soy ana

--- src/home/home.js
+++ src/home/home.js
@@ -1,11 +1,9 @@
-export const home = ()=>{
-    return(
-        
-        <div className="Home"> este es el Home</div>
-        <h1>hola</h1>
-
-        
-
-        
-    )
-}
+export const home = () => {
+  return (
+    <div>
+      <div className="Home"> este es el Home</div>
+      <h1>Hola</h1>
+      <p>Soy Ana</p>
+    </div>
+  );
+};
